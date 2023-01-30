3453a201c185af53d123b1a880caa1aff25b112e
Jan de Visser <jan@de-visser.net>
2023-01-30 09:49:25 -0500

    Improved test of CustomScanner.

--- lexer/test/CustomScannerTest.cpp
+++ lexer/test/CustomScannerTest.cpp
@@ -36,10 +36,13 @@ Line 3
 Line 4)");
     EXPECT_EQ(tokens.size(), 7);
     EXPECT_EQ(tokens[0].code(), TokenCode::Text);
+    EXPECT_EQ(tokens[0].value(), "Line 1");
     EXPECT_EQ(tokens[1].code(), TokenCode::NewLine);
     EXPECT_EQ(tokens[2].code(), TokenCode::NewLine);
     EXPECT_EQ(tokens[3].code(), TokenCode::Text);
+    EXPECT_EQ(tokens[3].value(), "Line 3");
     EXPECT_EQ(tokens[4].code(), TokenCode::NewLine);
     EXPECT_EQ(tokens[5].code(), TokenCode::Text);
+    EXPECT_EQ(tokens[5].value(), "Line 4");
     EXPECT_EQ(tokens[6].code(), TokenCode::EndOfFile);
 }
