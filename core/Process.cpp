c60fa4a5165ae8b9916cb4979a6cbd125f4ed7c6
Jan de Visser <jan@de-visser.net>
2023-04-03 14:02:30 -0400

    Incremental improvements to Process
    Mostly around running the child in parallel and reading stdout and stderr
    asynchronously.

--- core/Process.cpp
+++ core/Process.cpp
@@ -4,9 +4,11 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <csignal>
 #include <cstring>
 #include <iostream>
-#include <sys/fcntl.h>
+#include <poll.h>
+#include <signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -15,7 +17,185 @@
 
 namespace Obelix {
 
-ErrorOr<int,SystemError> execute(std::string const& cmd, std::vector<std::string> const& args)
+constexpr int PipeEndRead = 0;
+constexpr int PipeEndWrite = 1;
+
+Pipe::~Pipe()
+{
+    close();
+}
+
+ErrorOr<void,SystemError> Pipe::create()
+{
+    if (pipe(m_pipe) == -1) {
+        return SystemError { ErrorCode::IOError, "pipe() failed" };
+    }
+    return {};
+}
+
+void Pipe::close()
+{
+    if (m_fd >= 0)
+        ::close(m_fd);
+    m_fd = -1;
+}
+
+void pipe_thread(void *p)
+{
+    auto pipe = static_cast<ReadPipe*>(p);
+    pipe->read();
+}
+
+ReadPipe::~ReadPipe()
+{
+}
+
+void ReadPipe::connect_parent()
+{
+    m_fd = m_pipe[PipeEndRead];
+    ::close(m_pipe[PipeEndWrite]);
+    fcntl(m_fd, F_SETFL, O_NONBLOCK);
+    m_thread = new std::thread(&ReadPipe::read, this);
+    m_thread->detach();
+    delete m_thread;
+}
+
+void ReadPipe::connect_child(int fd)
+{
+    while ((dup2(m_pipe[PipeEndWrite], fd) == -1) && (errno == EINTR)) { }
+    ::close(m_pipe[PipeEndRead]);
+    ::close(m_pipe[PipeEndWrite]);
+}
+
+void ReadPipe::read()
+{
+    struct pollfd poll_fd {0};
+    poll_fd.fd = fd();
+    poll_fd.events = POLLIN;
+
+    while (true) {
+        if (poll(&poll_fd, 1, 0) == -1) {
+//            if (errno == EINTR)
+//                continue;
+            std::cerr << "poll(stdin, stdout) failed: " << strerror(errno) << "\n";
+            break;
+        }
+        if (poll_fd.revents & POLLIN)
+            drain();
+    }
+    close();
+};
+
+void ReadPipe::drain()
+{
+    std::lock_guard<std::mutex> lock(m_lock);
+    uint8_t buffer[4096];
+    std::string current;
+    for (auto count = ::read(m_fd, buffer, sizeof(buffer) - 1); count != 0; count = ::read(m_fd, buffer, sizeof(buffer) - 1)) {
+        if (count > 0) {
+            bool prev_was_cr { false };
+            for (auto ix = 0u; ix < count; ++ix) {
+                auto ch = static_cast<char>(buffer[ix]);
+                auto newline = [&current,this]() {
+                    m_lines.push_back(std::move(current));
+                    current.clear();
+                };
+                switch (ch) {
+                case '\r':
+                    newline();
+                    prev_was_cr = true;
+                    break;
+                case '\n':
+                    if (!prev_was_cr) {
+                        newline();
+                    }
+                    prev_was_cr = true;
+                    break;
+                default:
+                    current += ch;
+                    prev_was_cr = false;
+                }
+            }
+            continue;
+        }
+        switch (errno) {
+        case EINTR:
+            continue;
+        case EBADF:
+        case EAGAIN:
+            goto exit_loop;
+        default:
+            std::cerr << SystemError { ErrorCode::IOError, "Error reading child process output" }.to_string() << "\n";
+            goto exit_loop;
+        }
+    }
+exit_loop:
+    if (!current.empty())
+        m_lines.push_back(std::move(current));
+    m_condition.notify_all();
+};
+
+std::vector<std::string> ReadPipe::lines()
+{
+    std::lock_guard<std::mutex> const lock(m_lock);
+    std::vector<std::string> ret;
+    std::cout << "<----[" << fd() << "]\n";
+    while (!m_lines.empty()) {
+        auto l = m_lines.front();
+        std::cout << l << "\n";
+        ret.push_back(std::move(l));
+        m_lines.pop_front();
+    }
+    return ret;
+}
+
+void ReadPipe::expect()
+{
+    std::unique_lock lock(m_lock);
+    while (true) {
+        if (!m_lines.empty())
+            return;
+        m_condition.wait(lock);
+    }
+}
+
+void WritePipe::connect_parent()
+{
+    m_fd = m_pipe[PipeEndWrite];
+    ::close(m_pipe[PipeEndRead]);
+}
+
+void WritePipe::connect_child(int fd)
+{
+    while ((dup2(m_pipe[PipeEndRead], fd) == -1) && (errno == EINTR)) { }
+    ::close(m_pipe[PipeEndRead]);
+    ::close(m_pipe[PipeEndWrite]);
+}
+
+ErrorOr<int, SystemError> WritePipe::write(std::string const& str)
+{
+    return write((uint8_t *) str.c_str(), str.length());
+}
+
+ErrorOr<int, SystemError> WritePipe::write(std::string_view const& str)
+{
+    return write((uint8_t *) str.data(), str.length());
+}
+
+ErrorOr<int, SystemError> WritePipe::write(uint8_t const* buffer, size_t bytes)
+{
+    ssize_t count;
+    while (true) {
+        count = ::write(m_fd, buffer, bytes);
+        if (count >= 0)
+            break;
+        if (errno != EINTR)
+            return SystemError { ErrorCode::IOError, "Error writing to child process input" };
+    }
+    return count;
+}
+
+ErrorOr<int, SystemError> execute(std::string const& cmd, std::vector<std::string> const& args)
 {
     Process p(cmd, args);
     return p.execute();
@@ -27,7 +207,51 @@ Process::Process(std::string command, std::vector<std::string> arguments)
 {
 }
 
-ErrorOr<int,SystemError> Process::execute()
+std::vector<std::string> Process::standard_out()
+{
+    return m_stdout.lines();
+}
+
+std::vector<std::string> Process::standard_error()
+{
+    return m_stderr.lines();
+}
+
+ErrorOr<int, SystemError> Process::execute()
+{
+    TRY_RETURN(start());
+    return this->wait();
+}
+
+ErrorOr<void, SystemError> Process::background()
+{
+    TRY_RETURN(start());
+    return {};
+}
+
+ErrorOr<int, SystemError> Process::wait()
+{
+    if (m_pid == 0)
+        return 0;
+    int exit_code;
+    if (waitpid(m_pid, &exit_code, 0) == -1 && errno != ECHILD && errno != EINTR)
+        return SystemError { ErrorCode::IOError, "waitpid() failed" };
+    m_pid = 0;
+    m_stdin.close();
+    if (!WIFEXITED(exit_code))
+        return SystemError { ErrorCode::IOError, "Child program {} crashed due to signal {}", m_command, WTERMSIG(exit_code) };
+    return WEXITSTATUS(exit_code);
+}
+
+ErrorOr<void, SystemError> Process::terminate()
+{
+    m_stdin.close();
+    if (auto err = wait(); err.is_error())
+        return err.error();
+    return {};
+}
+
+ErrorOr<void, SystemError> Process::start()
 {
     auto sz = m_arguments.size();
     char** argv = new char*[sz + 2];
@@ -38,7 +262,8 @@ ErrorOr<int,SystemError> Process::execute()
         strcpy(argv[ix + 1], m_arguments[ix].c_str());
     }
     argv[sz + 1] = nullptr;
-    std::cout << format("[CMD] {} {}", m_command, join(m_arguments, ' ')) << '\n';
+    if (m_log)
+        std::cout << format("[CMD] {} {}", m_command, join(m_arguments, ' ')) << '\n';
 
     ScopeGuard sg([&argv, &sz]() {
         for (auto ix = 0u; ix < sz; ++ix)
@@ -46,46 +271,47 @@ ErrorOr<int,SystemError> Process::execute()
         delete[] argv;
     });
 
-    int filedes[2];
-    if (pipe(filedes) == -1) {
-        return SystemError { ErrorCode::IOError, "pipe() failed" };
-    }
+    signal(SIGCHLD, SIG_IGN);
 
-    pid_t pid = fork();
-    if (pid == -1)
+    TRY_RETURN(m_stdin.create());
+    TRY_RETURN(m_stdout.create());
+    TRY_RETURN(m_stderr.create());
+
+    m_pid = fork();
+    if (m_pid == -1)
         return SystemError { ErrorCode::IOError, "fork() failed" };
-    if (pid == 0) {
-        while ((dup2(filedes[1], STDOUT_FILENO) == -1) && (errno == EINTR)) { }
-        close(filedes[0]);
-        close(filedes[1]);
+    if (m_pid == 0) {
+        m_stdin.connect_child(STDIN_FILENO);
+        m_stdout.connect_child(STDOUT_FILENO);
+        m_stderr.connect_child(STDERR_FILENO);
         execvp(m_command.c_str(), argv);
         return SystemError { ErrorCode::IOError, "execvp() failed" };
     }
-    close(filedes[1]);
+    m_stdin.connect_parent();
+    m_stdout.connect_parent();
+    m_stderr.connect_parent();
+    return {};
+}
 
-    char buffer[4096];
-    while (true) {
-        auto count = read(filedes[0], buffer, sizeof(buffer) - 1);
-        if (count == -1) {
-            if (errno == EINTR) {
-                continue;
-            } else {
-                return SystemError { ErrorCode::IOError, "Error reading child process output" };
-            }
-        }
-        if (count == 0)
-            break;
-        buffer[count] = '\0';
-        m_stdout += buffer;
-    }
-    close(filedes[0]);
+bool Process::running() const
+{
+    return m_pid != 0;
+}
 
-    int exit_code;
-    if (waitpid(pid, &exit_code, 0) == -1)
-        return SystemError { ErrorCode::IOError, "waitpid() failed" };
-    if (!WIFEXITED(exit_code))
-        return SystemError { ErrorCode::IOError, "Child program {} crashed due to signal {}", m_command, WTERMSIG(exit_code) };
-    return WEXITSTATUS(exit_code);
+ErrorOr<int, SystemError> Process::write(std::string const& str)
+{
+    std::cout << "---->\n**" << str << "**\n";
+    return m_stdin.write(str);
+}
+
+ErrorOr<int, SystemError> Process::write(std::string_view const& str)
+{
+    return m_stdin.write(str);
+}
+
+ErrorOr<int, SystemError> Process::write(uint8_t const* buffer, size_t bytes)
+{
+    return m_stdin.write(buffer, bytes);
 }
 
 }
--- core/Process.h
+++ core/Process.h
@@ -6,7 +6,12 @@
 
 #pragma once
 
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <sstream>
 #include <string>
+#include <thread>
 
 #include <core/Error.h>
 
@@ -29,6 +34,47 @@ ErrorOr<int,SystemError> execute(std::string const& name, std::string const& arg
     return execute(name, cmd_args, std::forward<Args>(args)...);
 }
 
+class Pipe {
+public:
+    virtual ~Pipe();
+    ErrorOr<void,SystemError> create();
+    virtual void connect_parent() = 0;
+    virtual void connect_child(int) = 0;
+    void close();
+    [[nodiscard]] int fd() const { return m_fd; }
+
+protected:
+    int m_pipe[2] { -1, -1 };
+    int m_fd { -1 };
+};
+
+class ReadPipe : public Pipe {
+public:
+    ~ReadPipe();
+    void connect_parent() override;
+    void connect_child(int) override;
+    void read();
+    std::vector<std::string> lines();
+    void expect();
+
+private:
+    void drain();
+
+    std::deque<std::string> m_lines;
+    std::mutex m_lock;
+    std::condition_variable m_condition;
+    std::thread* m_thread;
+};
+
+class WritePipe : public Pipe {
+public:
+    void connect_parent() override;
+    void connect_child(int) override;
+    ErrorOr<int,SystemError> write(std::string const&);
+    ErrorOr<int,SystemError> write(std::string_view const&);
+    ErrorOr<int,SystemError> write(uint8_t const*, size_t);
+};
+
 class Process {
 public:
     explicit Process(std::string, std::vector<std::string>);
@@ -40,19 +86,23 @@ public:
         add_arguments(std::forward<Args>(args)...);
     }
 
-    [[nodiscard]] std::string const& standard_out() const
-    {
-        return m_stdout;
-    }
-
-    [[nodiscard]] std::string const& standard_error() const
-    {
-        return m_stderr;
-    }
-
     ErrorOr<int,SystemError> execute();
+    ErrorOr<void,SystemError> background();
+    ErrorOr<void,SystemError> terminate();
+    ErrorOr<int,SystemError> wait();
+    bool running() const;
+    [[nodiscard]] WritePipe& in() { return m_stdin; }
+    [[nodiscard]] ReadPipe& out() { return m_stdout; }
+    [[nodiscard]] ReadPipe& err() { return m_stderr; }
+    [[nodiscard]] std::vector<std::string> standard_out();
+    [[nodiscard]] std::vector<std::string> standard_error();
+    ErrorOr<int,SystemError> write(std::string const&);
+    ErrorOr<int,SystemError> write(std::string_view const&);
+    ErrorOr<int,SystemError> write(uint8_t const*, size_t);
 
 private:
+    ErrorOr<void,SystemError> start();
+
     template<typename... Args>
     void add_arguments(std::string const& argument, Args&&... args)
     {
@@ -66,8 +116,12 @@ private:
 
     std::string m_command;
     std::vector<std::string> m_arguments = {};
-    std::string m_stdout;
-    std::string m_stderr;
+    pid_t m_pid { 0 };
+    bool m_log { false };
+    WritePipe m_stdin;
+    ReadPipe m_stdout;
+    ReadPipe m_stderr;
+    std::mutex m_lock;
 };
 
 }
